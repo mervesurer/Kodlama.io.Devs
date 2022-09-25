3a4e9af72de65820b14a7bbb01bee4097dd7cb57
mervesurer <mervesureer@gmail.com>
2022-09-25 20:07:18 +0300

    week 3 homework 1

---
+++ src/kodlama.io.devs/Application/Features/Languages/Rules/LanguageBusinessRules.cs
@@ -0,0 +1,36 @@
+﻿using Application.Services;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Application.Features.Languages.Rules
+{
+    public class LanguageBusinessRules
+    {
+        private readonly ILanguageRepository _languageRepository;
+
+        public LanguageBusinessRules(ILanguageRepository languageRepository)
+        {
+            _languageRepository = languageRepository;
+        }
+
+        public async Task LanguageCanNotBeDublicateted(string name)
+        {
+            IPaginate<Language> result = await _languageRepository.GetListAsync(lang => lang.Name == name);
+            if (result.Items.Any())
+                throw new BusinessException("Language name exists.");
+        }
+
+        public async Task LanguageShouldExistWhenRequested(Language? language)
+        {
+            if (language == null)
+                throw new BusinessException("This Id does not exists.");
+        }
+    }
+}
---
+++ src/kodlama.io.devs/Application/Services/ILanguageRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public interface ILanguageRepository : IAsyncRepository<Language>, IRepository<Language>
+    {
+        
+    }
+}
